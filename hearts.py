ce5f11639fa87f7f09d039bc97d6ab2cb36999d5
wirelessr <ifloveyou2014@gmail.com>
2018-08-12 13:55:59 +0800

    Add for current states

--- hearts.py
+++ hearts.py
@@ -11,62 +11,101 @@ class HeartsEnv(gym.Env):
         self.observation_space = spaces.Tuple([
             # player states
             spaces.Tuple([
-                spaces.MultiDiscrete([
-                    200, # score
-                    1, # your turn
-                    4, # pos
-                ]),
+                spaces.Discrete(200), # score
                 spaces.Tuple([ # hand
                     spaces.MultiDiscrete([13, 4])
                 ] * 13),
                 spaces.Tuple([ # income
                     spaces.MultiDiscrete([13, 4])
                 ] * 52),
-            ] * n_seats),
+            ] * 4),
             # table states
             spaces.Tuple([
-                spaces.Discrete(n_pocket_cards), # rounds
-                spaces.Discrete(n_seats), # start pos
-                spaces.Tuple([ # draws
-                    spaces.MultiDiscrete([13, 4]),
-                ] * n_seats)
+                spaces.Discrete(13), # n_round
+                spaces.Discrete(4), # start_pos
+                spaces.Discrete(4), # cur_pos
+                spaces.Discrete(1), # exchanged
+                spaces.Discrete(1), # heart_occured
+                spaces.Discrete(100), # n_games
+                spaces.Tuple([ # board
+                    spaces.MultiDiscrete([13, 4])
+                ] * 4),
+                spaces.Tuple([ # first_draw
+                    spaces.MultiDiscrete([13, 4])
+                ]),
+                spaces.Tuple([ # bank
+                    spaces.Tuple([
+                        spaces.MultiDiscrete([13, 4])
+                    ] * 3),
+                ] * 4)
             ]),
         ])
-        # TODO
+
         self.action_space = spaces.Tuple([
-            spaces.Discrete(4),
-            spaces.Tuple([
-                spaces.Discrete(13),
-                spaces.Discrete(4),
-            ]),
+            spaces.Discrete(4), # cur_pos
+            spaces.Tuple([ # bank(3) draw(1)
+                spaces.MultiDiscrete([13, 4])
+            ] * 3),
         ])
+
+    def _pad(self, l, n, v):
+        if (not l) or (l is None):
+            l = []
+        return l + [v] * (n - len(l))
 
 
     # TODO
     def step(obs):
         return cur_obs, reward, isfinish, info
 
+    # XXX ...strange I fell confused about the return type
+    # What type is for MultiDiscrete XXX
     def _get_current_state(self):
         player_states = []
-        for idx, player in enumerate(self._player):
+        for idx, player in enumerate(self._table.players):
             player_features = [
                 int(player.score),
-                int(player.isYourTurn),
-                int(idx),
             ]
-            player_states.append((player_features, player.get_hand(), player.get_income())) # Tuple: [int, int], ([r, s], [r, s], ...), ([r, s], [r, s], ...)
-        table_states = ([
+            
+            player_hand = player.hand
+            self._pad(player_hand, 13, (-1, -1))
+
+            player_income = player.income
+            self._pad(player_income, 52, (-1, -1))
+
+            # Tuple: [int], ([r, s], [r, s], ...), ([r, s], [r, s], ...)
+            player_states.append((player_features, player_hand, player_income))
+
+        table_states = [
             int(self._table.n_round),
             int(self._table.start_pos)
-        ], self._table.board)
-        # XXX
-        return (tuple(player_states), table_states)
+            int(self._table.cur_pos),
+            int(self._table.exchange),
+            int(self._table.heart_occur),
+            int(self._table.n_games),
+        ]
+
+        boards = []
+        for card in self._table.board:
+            if card:
+                boards.append(card)
+            else:
+                boards.append((-1, -1))
+
+        first_draw = self._table.first_draw if self._table.first_draw else (-1, -1)
+
+        banks = []
+        for cards in self._table.bank:
+            bank = cards
+            self._pad(bank, 3, (-1, -1))
+            banks.append(tuple(bank))
+
+        table_states.append(boards, first_draw, banks)
+
+        return (tuple(player_states), tuple(table_states))
 
     def reset(self):
-        self._deck = deck
-        random.shuffle(self._deck)
-        self._player = [Player(i, self._deck[i*n_pocket_cards : (i+1)*n_pocket_cards]) for i in range(4)]
-        self._table = Table(self._player)
+        self._table = Table()
         return self._get_current_state()
 
 
