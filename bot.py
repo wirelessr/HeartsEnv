8d79726fb9aa9c92ba713ee6860a80068f258429
ChunTing Wu <chunting_wu@trendmicro.com>
2018-08-13 22:38:59 -0400

    Bot almost finished

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import random
+import logging
+
+from numpy import array
 
 from hearts.hearts import HeartsEnv
+
+logger = logging.getLogger(__name__)
 
 class RandomBot():
     def __init__(self, idx):
@@ -10,18 +15,38 @@ class RandomBot():
     def declare_action(self, player_obs, table_obs):
         action = [self.idx]
 
-        score, hand, income = player_obs
-        n_round, start_pos, cur_pos, exchaged, hearts_occur, n_game,\
-                board, first_draw, bank = table_obs
+        score, (hand,), (income,) = player_obs
+        n_round, start_pos, cur_pos, exchanged, hearts_occur, n_game,\
+                board, (first_draw,), bank = table_obs
         
-        hand_card = [(c[0], c[1]) for c in hand if c[0] != -1 or c[1] != -1]
-        board_card = [(c[0], c[1]) for c in board]
+        hand_card = [c for c in hand if c[0] != -1 or c[1] != -1]
+        board_card = [c for c in board]
 
         if not exchanged and n_game % 4 != 0:
             # 3 cards
             draws = random.sample(hand, 3)
         else:
             # 1 card
+            if self.idx == start_pos and n_round == 0:
+                draws = [array([0, 3])]
+            else:
+                print('first_draw', first_draw)
+                for card in hand_card:
+                    if card[1] == first_draw[1]:
+                        draws = [card]
+                        break
+                else:
+                    for card in hand_card:
+                        (rank, suit) = (card[0], card[1])
+                        if not hearts_occur and (rank, suit) != (10, 0) and suit != 1:
+                            print('rule')
+                            draws = [card]
+                            break
+                    else:
+                        print('random')
+                        draws = [random.choice(hand_card)]
+
+            draws += [array([-1, -1]), array([-1, -1])]
             
 
         # draws = [array([-1, -1]), array([-1, -1]), array([-1, -1])]
@@ -43,11 +68,12 @@ class BotProxy:
         while not done:
             self.env.render()
 
-            n_round, start_pos, cur_pos, exchaged, hearts_occur, n_game,\
+            n_round, start_pos, cur_pos, exchanged, hearts_occur, n_game,\
                     board, first_draw, bank = obs[1]
 
             player_obs = tuple([obs[0][i]] for i in range(cur_pos*3, cur_pos*3+3))
             action = self.bots[cur_pos].declare_action(player_obs, obs[1])
+            print(action)
             obs, rew, done, _ = self.env.step(action)
 
 if __name__ == '__main__':
